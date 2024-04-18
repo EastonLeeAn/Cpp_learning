f5b0c8a682769bb013597011901e98efdb9b8464
EastonLeeAn <physicsquantum163@gmail.com>
2024-04-18 20:59:56 +0800

    sortedSquare

--- Code whim/1.array/2remove.cpp
+++ Code whim/1.array/2remove.cpp
@@ -2,28 +2,55 @@
 #include <vector>
 using namespace std;
 
-class remove
+class vemove
 {
 private:
     /* data */
 public:
     int removeElement1(vector<int> &nums, int val /* args */)
     {
-        int size = nums.size();//数组的大小 Size of array
-        for (int i = 0; i < size; i++)
+        int size = nums.size();        // 数组的大小 Size of array
+        for (int i = 0; i < size; i++) // 遍历数组 Go through the groups
         {
-            if (nums[i] == val)
+            if (nums[i] == val) // 找到要的元素 Find the desired element
             {
-                for (int j = i + 1; j < size; j++)
+                for (int j = i + 1; j < size; j++) // 更新数组 Update array
                 {
                     nums[j - 1] = nums[j];
                 }
+                i--;
+                size--;
             }
-            i--;    
-            size--;
+
             // ~2remove();
+            // cout <<
         }
-        return 0;
+        return size;
+    }
+
+    int removeElement2(vector<int> &nums, int val)
+    {
+        int size = nums.size();
+        // fast and low pointers
+        int fast = 0;
+        int slow = 0;
+        while (fast < size)
+        {
+            if (nums[fast] != val)//注意 考虑是 等 还是 不等
+            //等的情况 和  不等的情况
+            {
+                nums[slow] = nums[fast];
+
+                // fast++; 
+                slow++; //slow 有条件的加
+            }
+
+            fast ++;//错误，fast 怎么可能在以一个循环里面多加几次
+            //fast 肯定是一直加
+
+        }
+
+        return slow;
     }
 };
 
@@ -33,13 +60,19 @@ int main()
     vector<int> obj;
     for (int i = 0; i < 10; ++i)
     {
-        obj.push_back(i); // 将 i 添加到 obj 中
+            obj.push_back(i); // 将 i 添加到 obj 中
     }
     cout << obj[5] << endl;
+    for (int num : obj)
+    {
+        cout << num << " ";
+    }
 
     // return 0;
-
+    cout << endl;
     // Solution mm;
+    vemove aa;
+    cout << aa.removeElement2(obj, 2) << endl;
 
     // cout << mm.search(obj,3) << endl ;
 
---
+++ Code whim/1.array/3sortedsquares.cpp
@@ -0,0 +1,118 @@
+/*
+给你一个按 非递减顺序 排序的整数数组 nums，
+返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+
+示例 1：
+
+输入：nums = [-4,-1,0,3,10]
+输出：[0,1,9,16,100]
+解释：平方后，数组变为 [16,1,0,9,100]，排序后，数组变为 [0,1,9,16,100]
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <algorithm>
+
+class Solution
+{
+public:
+    // 暴力解法 Violent solution
+
+    vector<int> sortedSquares(vector<int> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            A[i] *= A[i];
+        }
+        sort(A.begin(), A.end()); // 快速排序
+        return A;
+    }
+    // 双指针 Double pointer
+
+    vector<int> sortedSquares2(vector<int> &A)
+    {
+        int left = 0;
+        int right = A.size() - 1;
+        int i = A.size();
+        vector<int> B(i);
+        // int index = 0;
+        //while循环注意 边界
+
+        while (left <= right)
+        {
+            if (A[left] * A[left] < A[right] * A[right])
+            {
+                // B.push_back(A[left] * A[left]);
+                // B.push_back(A[right] * A[right]);
+                B[i-1]=A[right] * A[right];
+                right --;
+               
+            }
+            else
+            {
+                // B.push_back(A[right] * A[right]);
+                // B.push_back(A[left] * A[left]);
+                B[i-1]=A[left] * A[left];
+                 left ++;
+            }
+            i--;
+        }
+        return B;
+    }
+};
+//answer
+
+class Solutionaa {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        int k = A.size() - 1;
+        vector<int> result(A.size(), 0);
+        for (int i = 0, j = A.size() - 1; i <= j;) { // 注意这里要i <= j，因为最后要处理两个元素
+            if (A[i] * A[i] < A[j] * A[j])  {
+                result[k--] = A[j] * A[j];
+                j--;
+            }
+            else {
+                result[k--] = A[i] * A[i];
+                i++;
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    // 在这里编写你的代码
+    vector<int> obj;
+    for (int i = -4; i < 10; ++i)
+    {
+        obj.push_back(i); // 将 i 添加到 obj 中
+    }
+    cout << obj[5] << endl;
+    for (int num : obj)
+    {
+        cout << num << " ";
+    }
+
+    // return 0;
+    cout << endl;
+
+    Solution aa;
+    cout << endl;
+    // cout << aa.sortedSquares2(obj) << endl;
+    // inta a = ;
+    for (int num : aa.sortedSquares2(obj))
+    {
+        std::cout << num << " ";
+    }
+    // cout <<obj.begin() obj.end();
+    //     for (auto it = obj.begin(); it != obj.end(); ++it) {
+    //     std::cout << *it << " ";
+    // }
+    // Solution mm;
+    // cout << mm.search(obj,3) << endl ;
+
+    cout << "end";
+    return 0; // 返回0表示程序成功结束
+}
